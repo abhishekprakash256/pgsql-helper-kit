6f2653ad6eded2891cd43215d6ea46e459a2b7df
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-23 18:19:30 -0400

    engine session reversed

--- pgsql_helper_kit/pgsql_crud_operations.py
+++ pgsql_helper_kit/pgsql_crud_operations.py
@@ -18,7 +18,7 @@ class Db_Helper():
     The class to make the CRUD operations
     """
 
-    def __init__(self, engine ,session):
+    def __init__(self,session, engine):
         self.session = session
         self.engine = engine
 
